2deabb82c2cdfdc37d0c12a008786a81a5c72f86
harimindi <34067134+harimindi@users.noreply.github.com>
2018-11-11 21:28:12 +0000

    Update Blink_Kivy.py

--- Blink_Kivy.py
+++ Blink_Kivy.py
@@ -4,11 +4,11 @@ Created on Fri Nov  2 20:48:24 2018
 
 @author: hari
 """
-import kivy.core.text
 from scipy.spatial import distance as dist
 from imutils.video import FileVideoStream, FPS, VideoStream
 from imutils import face_utils
 import imutils
+import argparse
 import numpy as np
 import time
 import dlib
@@ -16,89 +16,64 @@ import cv2
 
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.image import Image
 from kivy.uix.camera import Camera
 from kivy.lang.builder import Builder
 
 Builder.load_string('''
 <Button>:
     background_normal: ''
-    #background_color: 0,0,0,0
+    background_color: 0,0,0,0
     canvas.before:
         Color:
             rgba: (0.733, 0.145, 0.145,1) if self.state=='normal' else (0,.7,.7,1)
         RoundedRectangle:
             pos: self.pos
             size: self.size
-            radius: [20,]
-    background_color: 0.733, 0.145, 0.145,1.8
+            radius: [30,]
+    #background_color: 0.733, 0.145, 0.145,1.8
     font_size: 40
     color: 1,1,1,1
     size_hint: 0.3, 0.1
 <Layout>:
         
-    orientation: 'vertical'
     #canvas.before:
-        #Color:
-            #rgba:0.161, 0.561, 0.761,1
-        #Rectangle:
-            #pos: self.pos
-            #size: self.size
-    KivyCamera:
-        id: kivycam
+     #   Color:
+      #      rgba:0.161, 0.561, 0.761,1
+       # Rectangle:
+        #    pos: self.pos
+         #   size: self.size
+    #Camera:
+        #id: camera
         #resolution: (300, 300)
         #play: False
     
+    orientation: 'vertical'
     Button:
         id: blink
         text: "Blink"
-        pos_hint: {'x':.1, 'y':.1}
+        pos_hint: {'x':.1, 'y':.8}
         on_press: root.onCameraClick()
     Button:
         id: next
-        text: "Quit"
-        pos_hint: {'x':.6, 'y':.1}
-        on_press: root.doexit()
+        text: "Exit"
+        pos_hint: {'x':.6, 'y':.8}
+    Label:
+        id: label
+        text: "Take a Blink Selfie..!"
+        pos_hint: {'x':.8, 'y':.3}
 ''')
 
-class KivyCamera(Image):
-
-    def __init__(self, **kwargs):
-        super(KivyCamera, self).__init__(**kwargs)
-        self.capture = None
-
-    def start(self, capture, fps=30):
-        self.capture = capture
-        Clock.schedule_interval(self.update, 1.0 / fps)
-
-    def stop(self):
-        Clock.unschedule_interval(self.update)
-        self.capture = None
-
-    def update(self, dt):
-        return_value, frame = self.capture.read()
-        if return_value:
-            texture = self.texture
-            w, h = frame.shape[1], frame.shape[0]
-            if not texture or texture.width != w or texture.height != h:
-                self.texture = texture = Texture.create(size=(w, h))
-                texture.flip_vertical()
-            texture.blit_buffer(frame.tobytes(), colorfmt='bgr')
-            self.canvas.ask_update()
-
-
-capture = None
-
-class Layout(BoxLayout):
-    def init_camtest(self):
-        pass
-
+class Layout(FloatLayout):
     def onCameraClick(self, *args):
-        #blinkbtn = self.ids['blink']
+        #blink = self.ids['blink']
+        #camera = self.ids['camera']
+        #camera.play = True
         #camera.export_to_png("C:/Hari Docs/Kivy/pic.png")
         #print('Picure Taken')
-        global capture
+
+        
         def eye_aspect_ratio(eye):
             A = dist.euclidean(eye[1], eye[5])
             B = dist.euclidean(eye[2], eye[4])
@@ -113,7 +88,7 @@ class Layout(BoxLayout):
         COUNTER = 0
         TOTAL = 1
         
-        shape_predictor = 'C:\Hari Docs\python\Installers\shape_predictor_68_face_landmarks.dat'
+        shape_predictor = 'F:/Data_Science/FaceRecognition/shape_predictor_68_face_landmarks.dat'
         detector = dlib.get_frontal_face_detector()
         predictor = dlib.shape_predictor(shape_predictor)
         
@@ -121,15 +96,14 @@ class Layout(BoxLayout):
         (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS['right_eye']
         
         print("[INFO] starting video stream thread...")
-        capture = VideoStream(src=0).start()
-        self.ids.kivycam.start(capture)
+        vs = VideoStream(src=0).start()
         fileStream = False
         time.sleep(1.0)
         #fps= FPS().start()
-        #cv2.namedWindow('Capture', cv2.WINDOW_NORMAL)
-        
-        while True:
-            frame = capture.read()
+        cv2.namedWindow('Capture', cv2.WINDOW_NORMAL)
+        flag = 1
+        while (flag == 1):
+            frame = vs.read()
             frame = imutils.resize(frame, width=450)
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
          
@@ -155,32 +129,29 @@ class Layout(BoxLayout):
                 else:
                     if COUNTER >= EYE_AR_CONSEC_FRAMES:
                         print('Blink detected, stay steady and look at the camera...!')
-                        while TOTAL <= 3:
-                            frame = capture.read()
-                            img_name =  'opencv_frame_{}.png'.format(TOTAL)
-                            cv2.imwrite(img_name, frame)    
-                            print('{} written'.format(img_name))
-                            TOTAL += 1
-                            time.sleep(1)
+                        #while TOTAL <= 3:
+                        frame = vs.read()
+                        time.sleep(5) 
+                        img_name =  'opencv_frame_{}.png'.format(TOTAL)
+                        cv2.imwrite(img_name, frame)    
+                        print('{} written'.format(img_name))
+                        #TOTAL += 1                                               
                         #fps.stop()
-                        cv2.destroyAllWindows()
-                        blink.stop()
-                        #vs.stop()
-                        exit()
+                        #vs.stream.release()
+                        flag = 0                        
                     COUNTER = 0  # reset the eye frame counter
-            
-    def doexit(self):
-        global capture
-        if capture != None:
-            capture.release()
-            capture = None
-        EventLoop.close()
+            cv2.imshow('Capture', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        vs.stop()
+        cv2.destroyAllWindows()        
+        label = self.ids['label']
+        label.text = 'Picture Taken'        
 
 class myKivy(App):
     def build(self):
-        homeWin = Layout()
-        homeWin.init_camtest()
-        return homeWin
+        
+        return Layout()
 
 if __name__=='__main__':
     myKivy().run()
