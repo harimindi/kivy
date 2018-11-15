946c8aa793e9bca01ec9bf18a7d95fd83c2fe869
harimindi <34067134+harimindi@users.noreply.github.com>
2018-11-15 16:56:26 +0000

    Update Form1.py

--- Form1.py
+++ Form1.py
@@ -4,37 +4,48 @@ from kivy.lang.builder import Builder
 from kivy.config import Config
 from kivy.core.window import Window
 #Window.size = (400,130)
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
 
 Builder.load_string('''
 <MyWidget>:
-    canvas:
-        Color: 
-            rgba: (.5, 0.5, 0.93, 1)
-        Rectangle:
-            pos: self.pos
-            size: self.size
+    #canvas:
+        #Color: 
+            #rgba: (.5, 0.5, 0.5, 1)
+        #Rectangle:
+         #   pos: self.pos
+          #  size: self.size
     
-    TextInput:
-        size: 130, 30
-        pos: 70, 30
-        #text: root.help_message
-        background: 1., 1., 1., 1.
-        font_size: 14
-        multiline: False
+    RelativeLayout:
 
-    Label:
-        canvas.before:
-            Color:
-                rgba: 1,1,1,1
-        size: 200, 30
-        text: 'First Name'
-        pos: 0, 80
-        color: 1,1,1,1
-        font_size: 14
+        TextInput:
+            #size: 400, 50
+            pos: 400, 50
+            #text: root.help_message
+            background: 1., 1., 1., 1.
+            font_size: 14
+            multiline: False
+
+        Label:
+            background_color:
+            canvas.before:
+                Color:
+                    rgba: 0.5,0.5,0.5,1
+                Rectangle:
+                    pos: self.pos
+                    size: self.size
+            size: 300, 50
+            #size_hint: (None, None)
+            #pos_hint: {'x':.4, 'y':.4}
+            text: 'First Name'
+            text_size: root.width, None
+            pos: 30, 50
+            color: 1,1,1,1
+            font_size: 14
 
 ''')
 #MyLayout
