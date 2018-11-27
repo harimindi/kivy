9d6740b66c7b97ddf78f6a1aa3f7996ca177ec5d
harimindi <34067134+harimindi@users.noreply.github.com>
2018-11-27 16:33:43 +0000

    Update OCR.py

--- OCR.py
+++ OCR.py
@@ -6,6 +6,24 @@ import cv2
 import os
 import numpy as np
 
+def textparse(text):
+    country = text[2:5]
+    names = text[5:44]
+    docnumber = text[44:54]
+    checkdigit1 = text[54:55]
+    nationality = text[55:58]
+    dob_raw = text[58:64]
+    checkdigit2 = text[64:65]
+    sex = text[65:66]
+    expiry_raw = text[66:72]
+    checkdigit3 = text[72:73]
+    personalnum_raw = text[73:87]
+    checkdigit4 = text[87:88]
+    checkdigit5 = text[88:89]
+    names = names.split('<<')
+    lastname = names[0]
+    givenname = names[1]
+    print('IssuingCountry: {}\nLastName: {}\nGivenName: {}\nDocumentNumber: {}\nNationality: {}\nDate of Birth: {}\nSex: {}\nDate of Expiry: {}'.format(country,lastname, givenname,docnumber,nationality,dob_raw, sex,expiry_raw))
 #contruct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--input", required = True,
@@ -25,5 +43,18 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 #Thesholding
 gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]  
 
-text = pytesseract.image_to_string(gray)
-print(text)
+ocr = pytesseract.image_to_string(gray)
+print(ocr)
+textparse(ocr)
+
+    #print(lastname, givenname)
+    #print(docnumber)
+    #print(checkdigit1)
+    #print(nationality)
+    #print(dob_raw)
+    #print(checkdigit2)
+    #print(sex)
+    #print(expiry_raw)
+
+#print ('Number of Lines: {}'.format(len(text.split('\n'))))
+#[print(len(line)) for line in text.split('\n')]
